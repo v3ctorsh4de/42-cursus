b799b638507d71fb745835dd97cf41e63892222e
jreyes-s <me@example.com>
2026-01-13 11:28:31 +0100

    ft_memcpy,ft_memmove,README.md

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jreyes-s <jreyes-s@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 10:53:15 by jreyes-s          #+#    #+#             */
+/*   Updated: 2026/01/13 11:14:23 by jreyes-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	const unsigned char	*s;
+
+	d = dest;
+	s = src;
+	while (n-- > 0)
+		*(d++) = *(s++);
+	return (dest);
+}
