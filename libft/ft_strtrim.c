d7614666a424ff0e51a264428371ed9d7bfa9924
ch3f <jreyes-s@student.42madrid.com>
2026-01-29 19:27:34 +0000

    update: ft_bzero

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,22 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jreyes-s <jreyes-s@student.42madrid.c      +#+  +:+       +#+        */
+/*   By: jreyes-s <jreyes-s@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/13 10:37:07 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/01/29 19:27:16 by jreyes-s         ###   ########.fr       */
+/*   Created: 2026/01/29 18:36:02 by jreyes-s          #+#    #+#             */
+/*   Updated: 2026/01/29 18:36:25 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
-{
-	unsigned char	*ptr;
 
-	ptr = (unsigned char *)s;
-	while (n-- > 0)
-		*(ptr++) = 0;
-}
