0ce360b708f090774fb427c3c42b0cf035263a2b
ch3f <jreyes-s@student.42madrid.com>
2026-02-02 18:36:03 +0100

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -5,27 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jreyes-s <jreyes-s@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/30 02:56:47 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/01/30 15:19:29 by jreyes-s         ###   ########.fr       */
+/*   Created: 2026/02/02 18:27:14 by jreyes-s          #+#    #+#             */
+/*   Updated: 2026/02/02 18:35:39 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-static size_t	ft_count_words(char const *s, char c)
+static size_t	ft_count_words(char *s, char c)
 {
 	size_t	i;
 	size_t	words;
 
 	i = 0;
 	words = 0;
-	while (s[i])
+	while(s[i])
 	{
 		if (s[i] != c)
 		{
 			words++;
-			while (s[i] && s[i] != c)
+			while (s[i] & s[i] != c)
 				i++;
 		}
 		else
@@ -36,24 +35,7 @@ static size_t	ft_count_words(char const *s, char c)
 
 char	**ft_split(char const *s, char c)
 {
-	size_t		i;
-	size_t		words;
-	char	**str;
+	unsigned char	**str;
 
-	words = ft_count_words(s, c);
-	str = malloc(sizeof(*char) * words + 1);
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (s[i])
-	{
-
-	}
-	str[words + 1] = "\0";
-}
-
-int	main(void)
-{
-	ft_split("Hola    Mundo! aasdff", ' ');
-	return (0);
+	str = (unsigned char **)malloc
 }
