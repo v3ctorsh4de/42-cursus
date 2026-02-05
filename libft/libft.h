26839715b1efedc1b01664647d44ee71522bb028
ch3f <jreyes-s@student.42madrid.com>
2026-02-05 17:08:59 +0100

    add: bonus to makefile

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jreyes-s <jreyes-s@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 20:52:31 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/02/05 16:23:55 by jreyes-s         ###   ########.fr       */
+/*   Updated: 2026/02/05 17:07:21 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,41 +17,47 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int				ft_isalpha(int c);
-size_t			ft_strlcat(char *dest, const char *src, size_t size);
-int				ft_isdigit(int c);
-int				ft_isalnum(int c);
-int				ft_isascii(int c);
-int				ft_isprint(int c);
-int				ft_tolower(int c);
-int				ft_toupper(int c);
-size_t			ft_strlen(const char *s);
-char			*ft_strchr(const char *string, int c);
-char			*ft_strrchr(const char *string, int c);
-int				ft_strncmp(const char *string1, const char *string2,
-					size_t count);
-char			*ft_strnstr(const char *haystack, const char *needle,
-					size_t len);
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
-void			*ft_memset(void *s, int c, size_t n);
-void			ft_bzero(void *s, size_t n);
-void			*ft_memcpy(void *dest, const void *src, size_t n);
-void			*ft_memmove(void *dest, const void *src, size_t n);
-void			*ft_memchr(const void *buf, int c, size_t count);
-int				ft_memcmp(const void *str1, const void *str2, size_t n);
-int				ft_atoi(const char *str);
-char			*ft_substr(char const *s, unsigned int start, size_t len);
-char			*ft_strjoin(char const *s1, char const *s2);
-char			*ft_strtrim(char const *s1, char const *set);
-char			**ft_split(char const *s, char c);
-void			*ft_calloc(size_t nmemb, size_t size);
-char			*ft_strdup(const char *s);
-char			*ft_itoa(int n);
-char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void			ft_striteri(char *s, void (*f)(unsigned int, char*));
-void			ft_putchar_fd(char c, int fd);
-void			ft_putstr_fd(char *s, int fd);
-void			ft_putendl_fd(char *s, int fd);
-void			ft_putnbr_fd(int n, int fd);
+int					ft_isalpha(int c);
+size_t				ft_strlcat(char *dest, const char *src, size_t size);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_tolower(int c);
+int					ft_toupper(int c);
+size_t				ft_strlen(const char *s);
+char				*ft_strchr(const char *string, int c);
+char				*ft_strrchr(const char *string, int c);
+int					ft_strncmp(const char *string1, const char *string2,
+						size_t count);
+char				*ft_strnstr(const char *haystack, const char *needle,
+						size_t len);
+unsigned int		ft_strlcpy(char *dest, char *src, unsigned int size);
+void				*ft_memset(void *s, int c, size_t n);
+void				ft_bzero(void *s, size_t n);
+void				*ft_memcpy(void *dest, const void *src, size_t n);
+void				*ft_memmove(void *dest, const void *src, size_t n);
+void				*ft_memchr(const void *buf, int c, size_t count);
+int					ft_memcmp(const void *str1, const void *str2, size_t n);
+int					ft_atoi(const char *str);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
+char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_strtrim(char const *s1, char const *set);
+char				**ft_split(char const *s, char c);
+void				*ft_calloc(size_t nmemb, size_t size);
+char				*ft_strdup(const char *s);
+char				*ft_itoa(int n);
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void				ft_striteri(char *s, void (*f)(unsigned int, char *));
+void				ft_putchar_fd(char c, int fd);
+void				ft_putstr_fd(char *s, int fd);
+void				ft_putendl_fd(char *s, int fd);
+void				ft_putnbr_fd(int n, int fd);
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 #endif
