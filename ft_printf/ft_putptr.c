2abcde920efff0bde7ac8e098c95e8be76acd66f
ch3f <jreyes-s@student.42madrid.com>
2026-02-10 00:37:21 +0100

    fix some functions

--- ft_printf/ft_putptr.c
+++ ft_printf/ft_putptr.c
@@ -6,11 +6,28 @@
 /*   By: jreyes-s <jreyes-s@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 23:17:52 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/02/10 00:34:27 by jreyes-s         ###   ########.fr       */
+/*   Updated: 2026/02/10 00:37:09 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	ft_puthex_ulong(unsigned long n, int uppercase)
+{
+	char	*hex_digits;
+	int		count;
+
+	count = 0;
+	if (uppercase)
+		hex_digits = "0123456789ABCDEF";
+	else
+		hex_digits = "0123456789abcdef";
+	if (n >= 16)
+		count += ft_puthex_ulong(n / 16, uppercase);
+	count += ft_putchar(hex_digits[n % 16]);
+	return (count);
+}
+
 
 int	ft_putptr(void *ptr)
 {
@@ -19,12 +36,12 @@ int	ft_putptr(void *ptr)
 
 	count = 0;
 	count += ft_putstr("0x");
-	if (!ptr)
+	if (!ptr || ptr == 0)
 	{
 		count += ft_putchar('0');
 		return (count);
 	}
 	addr = (unsigned long)ptr;
-	count += ft_puthex(addr, 0);
+	count += ft_puthex_ulong(addr, 0);
 	return (count);
 }
