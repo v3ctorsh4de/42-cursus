95057e19a4ffb3fbd143ecd43386153e103ce751
ch3f <jreyes-s@student.42madrid.com>
2026-02-10 00:56:10 +0100

    fix some functions

--- ft_printf/ft_putptr.c
+++ ft_printf/ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: jreyes-s <jreyes-s@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 23:17:52 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/02/10 00:37:09 by jreyes-s         ###   ########.fr       */
+/*   Updated: 2026/02/10 00:53:58 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,16 @@ static int	ft_puthex_ulong(unsigned long n, int uppercase)
 	return (count);
 }
 
-
 int	ft_putptr(void *ptr)
 {
 	unsigned long	addr;
 	int				count;
 
+	if (!ptr)
+		return (ft_putstr("(nil)"));
 	count = 0;
-	count += ft_putstr("0x");
-	if (!ptr || ptr == 0)
-	{
-		count += ft_putchar('0');
-		return (count);
-	}
 	addr = (unsigned long)ptr;
+	count += ft_putstr("0x");
 	count += ft_puthex_ulong(addr, 0);
 	return (count);
 }
