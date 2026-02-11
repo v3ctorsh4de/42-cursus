ff4f9ca3fa49d1f04a3db861d990b42a386d7d94
ch3f <jreyes-s@student.42madrid.com>
2026-02-11 18:57:18 +0100

    changes

--- libft/ft_lstnew_bonus.c
+++ libft/ft_lstnew_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jreyes-s <jreyes-s@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 14:31:20 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/02/06 15:29:36 by jreyes-s         ###   ########.fr       */
+/*   Updated: 2026/02/11 18:57:04 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*new;
 
 	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
 	new->content = content;
 	new->next = NULL;
 	return (new);
