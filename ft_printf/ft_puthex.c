9bdec0101617b897a3be3e7e9364744f592e21e3
ch3f <jreyes-s@student.42madrid.com>
2026-02-14 19:11:10 +0100

    update ft_printf

--- ft_printf/ft_puthex.c
+++ ft_printf/ft_puthex.c
@@ -6,11 +6,19 @@
 /*   By: jreyes-s <jreyes-s@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 22:06:56 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/02/10 00:32:54 by jreyes-s         ###   ########.fr       */
+/*   Updated: 2026/02/14 19:10:36 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	safe_add(int *count, int ret)
+{
+	if (ret == -1)
+		return (-1);
+	*count += ret;
+	return (0);
+}
 
 int	ft_puthex(unsigned int n, int uppercase)
 {
@@ -25,8 +33,10 @@ int	ft_puthex(unsigned int n, int uppercase)
 	else
 		hex_digits = "0123456789abcdef";
 	if (nbr >= 16)
-		count += ft_puthex(nbr / 16, uppercase);
-	count += ft_putchar(hex_digits[nbr % 16]);
+		if (safe_add(&count, ft_puthex(nbr / 16, uppercase)) == -1)
+			return (-1);
+	if (safe_add(&count, ft_putchar(hex_digits[nbr % 16])) == -1)
+		return (-1);
 	return (count);
 }
 /*
--- ft_printf/ft_putptr.c
+++ ft_printf/ft_putptr.c
@@ -6,11 +6,19 @@
 /*   By: jreyes-s <jreyes-s@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 23:17:52 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/02/10 00:53:58 by jreyes-s         ###   ########.fr       */
+/*   Updated: 2026/02/14 19:05:25 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	safe_add(int *count, int ret)
+{
+	if (ret == -1)
+		return (-1);
+	*count += ret;
+	return (0);
+}
 
 static int	ft_puthex_ulong(unsigned long n, int uppercase)
 {
@@ -23,8 +31,10 @@ static int	ft_puthex_ulong(unsigned long n, int uppercase)
 	else
 		hex_digits = "0123456789abcdef";
 	if (n >= 16)
-		count += ft_puthex_ulong(n / 16, uppercase);
-	count += ft_putchar(hex_digits[n % 16]);
+		if (safe_add(&count, ft_puthex_ulong(n / 16, uppercase)) == -1)
+			return (-1);
+	if (safe_add(&count, ft_putchar(hex_digits[n % 16])) == -1)
+		return (-1);
 	return (count);
 }
 
@@ -36,8 +46,10 @@ int	ft_putptr(void *ptr)
 	if (!ptr)
 		return (ft_putstr("(nil)"));
 	count = 0;
+	if (safe_add(&count, ft_putstr("0x")) == -1)
+		return (-1);
 	addr = (unsigned long)ptr;
-	count += ft_putstr("0x");
-	count += ft_puthex_ulong(addr, 0);
+	if (safe_add(&count, ft_puthex_ulong(addr, 0)) == -1)
+		return (-1);
 	return (count);
 }
