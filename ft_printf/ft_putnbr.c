0fe4d422ff3c44a03f48aaacd660a37f786e3bb5
ch3f <jreyes-s@student.42madrid.com>
2026-02-14 19:29:06 +0100

    update ft_putnbr

--- ft_printf/ft_putnbr.c
+++ ft_printf/ft_putnbr.c
@@ -6,38 +6,38 @@
 /*   By: jreyes-s <jreyes-s@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 21:19:40 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/02/14 17:43:42 by jreyes-s         ###   ########.fr       */
+/*   Updated: 2026/02/14 19:28:18 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	safe_add(int *count, int ret)
+{
+	if (ret == -1)
+		return (-1);
+	*count += ret;
+	return (0);
+}
+
 int	ft_putnbr(int n)
 {
 	long	nbr;
 	int		count;
-	int		tmp;
 
 	nbr = n;
 	count = 0;
 	if (nbr < 0)
 	{
-		if (ft_putchar('-') == -1)
+		if (safe_add(&count, ft_putchar('-')) == -1)
 			return (-1);
-		count++;
 		nbr = -nbr;
 	}
 	if (nbr >= 10)
-	{
-		tmp = ft_putnbr(nbr / 10);
-		if (tmp == -1)
+		if (safe_add(&count, ft_putnbr(nbr / 10)) == -1)
 			return (-1);
-		count += tmp;
-	}
-	tmp = ft_putchar((nbr % 10) + '0');
-	if (tmp == -1)
+	if (safe_add(&count, ft_putchar((nbr % 10) + '0')) == -1)
 		return (-1);
-	count += tmp;
 	return (count);
 }
 /*
