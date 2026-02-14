b448c6fe92098daff1c68b900bc1cc7dc7ae0474
ch3f <jreyes-s@student.42madrid.com>
2026-02-14 19:33:19 +0100

    update ft_putnbr_unsigned

--- ft_printf/ft_putnbr_unsigned.c
+++ ft_printf/ft_putnbr_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: jreyes-s <jreyes-s@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 21:28:32 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/02/14 19:32:38 by jreyes-s         ###   ########.fr       */
+/*   Updated: 2026/02/14 19:33:15 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ int	ft_putnbr_unsigned(unsigned int n)
 	if (nbr >= 10)
 		if (safe_add(&count, ft_putnbr_unsigned(nbr / 10)) == -1)
 			return (-1);
-	tmp = ft_putchar(nbr % 10 + '0');
 	if (safe_add(&count, ft_putchar((nbr % 10) + '0')) == -1)
 		return (-1);
 	return (count);
