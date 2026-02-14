f3578504ada708e446bd72bf10ee3e76016ceabd
ch3f <jreyes-s@student.42madrid.com>
2026-02-14 19:32:48 +0100

    update ft_putnbr_unsigned

--- ft_printf/ft_putnbr_unsigned.c
+++ ft_printf/ft_putnbr_unsigned.c
@@ -6,31 +6,33 @@
 /*   By: jreyes-s <jreyes-s@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 21:28:32 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/02/14 18:10:07 by jreyes-s         ###   ########.fr       */
+/*   Updated: 2026/02/14 19:32:38 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	safe_add(int *count, int ret)
+{
+	if (ret == -1)
+		return (-1);
+	*count += ret;
+	return (0);
+}
+
 int	ft_putnbr_unsigned(unsigned int n)
 {
 	long	nbr;
 	int		count;
-	int		tmp;
 
 	nbr = n;
 	count = 0;
 	if (nbr >= 10)
-	{
-		tmp = ft_putnbr_unsigned(nbr / 10);
-		if (tmp == -1)
+		if (safe_add(&count, ft_putnbr_unsigned(nbr / 10)) == -1)
 			return (-1);
-		count += tmp;
-	}
 	tmp = ft_putchar(nbr % 10 + '0');
-	if (tmp == -1)
+	if (safe_add(&count, ft_putchar((nbr % 10) + '0')) == -1)
 		return (-1);
-	count += tmp;
 	return (count);
 }
 /*
