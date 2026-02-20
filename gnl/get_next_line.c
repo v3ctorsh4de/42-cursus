dd30d057a8821d3376e18e0628677dfec9b85bef
Juan Alberto <jreyes-s@student.42madrid.com>
2026-02-20 01:54:53 +0100

    update gnl - refactor get_next_line and utils for improved readability and functionality

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jreyes-s <jreyes-s@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 20:01:33 by jreyes-s          #+#    #+#             */
-/*   Updated: 2026/02/20 01:23:30 by jreyes-s         ###   ########.fr       */
+/*   Updated: 2026/02/20 01:54:16 by jreyes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,18 @@ char	*get_next_line(int fd)
 		return (NULL);
 	if (fd < 0)
 		return (free(buf), NULL);
-	n = read(fd, buf, BUFFER_SIZE);
+	n = 1;
 	while (!ft_has_newline(stash) && n > 0)
 	{
+		n = read(fd, buf, BUFFER_SIZE);
+		if (n < 0)
+			return (free(buf), free(stash), stash = NULL, NULL);
 		buf[n] = '\0';
 		stash = ft_strjoin(stash, buf);
 	}
 	free(buf);
-	if (n < 0)
-		return (free(stash), stash = NULL, NULL);
+	if (!stash)
+		return (NULL);
 	line = ft_extract_line(stash);
 	stash = ft_update_stash(stash);
 	return (line);
